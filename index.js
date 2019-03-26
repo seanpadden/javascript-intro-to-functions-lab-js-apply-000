df45145a3145d7fc75b7b61bb60453d457c4dad5
Sean Padden <sean.padden7@gmail.com>
2019-03-26 18:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
     
 function logShout(string) {
   return logShout.toUpperCase();
-  console.log(logShout)
-}
+  }
+  console.log(logShout);
